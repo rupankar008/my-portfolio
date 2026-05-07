a46e3d3d8b58799dc730d55a106a5a2108b5f37e
Rupankar <vmtolegit@gmail.com>
2026-05-07 19:57:26 +0530

    fix: correct Spotify token endpoint URL

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -3,7 +3,7 @@ const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
 
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
-const TOKEN_ENDPOINT = `https://api.spotify.com/api/token`;
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
 const getAccessToken = async () => {
   if (!client_id || !client_secret || !refresh_token) {
